a63758f2973348d9afb703a2722fa152bac1c8cd
aakash sinha <aksinha2509@gmail.com>
2020-10-29 12:29:45 +0530

    Initial Commit

---
+++ SliceTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SliceTask
+//
+//  Created by Aakash Sinha on 29/10/20.
+//  Copyright © 2020 Aakash Sinha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
